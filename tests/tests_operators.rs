23df797fa59d1702c0754162caf1a62b9a35af2d
Benjamin Guichard <benjamin.guichard@epita.fr>
2024-05-28 12:23:16 +0200

    naming conventions

--- tests/tests_operators.rs
+++ tests/tests_operators.rs
@@ -6,7 +6,7 @@ mod tests_operators {
     use num_complex::Complex;
 
     #[test]
-    fn test_operator_H() {
+    fn test_operator_h() {
         let h_gate = Operator::one_qubit(OneQubitOp::H);
         assert_eq!(h_gate.data[0 * 2 + 0], Complex::new(FRAC_1_SQRT_2, 0.));
         assert_eq!(h_gate.data[0 * 2 + 1], Complex::new(FRAC_1_SQRT_2, 0.));
@@ -14,7 +14,7 @@ mod tests_operators {
         assert_eq!(h_gate.data[1 * 2 + 1], Complex::new(FRAC_1_SQRT_2, 0.));
     }
     #[test]
-    fn test_operator_X() {
+    fn test_operator_x() {
         let x_gate = Operator::one_qubit(OneQubitOp::X);
         assert_eq!(x_gate.data[0 * 2 + 0], Complex::new(0., 0.));
         assert_eq!(x_gate.data[0 * 2 + 1], Complex::new(1., 0.));
@@ -22,7 +22,7 @@ mod tests_operators {
         assert_eq!(x_gate.data[1 * 2 + 1], Complex::new(0., 0.));
     }
     #[test]
-    fn test_operator_Y() {
+    fn test_operator_y() {
         let y_gate = Operator::one_qubit(OneQubitOp::Y);
         assert_eq!(y_gate.data[0 * 2 + 0], Complex::new(0., 0.));
         assert_eq!(y_gate.data[0 * 2 + 1], Complex::new(0., -1.));
@@ -30,7 +30,7 @@ mod tests_operators {
         assert_eq!(y_gate.data[1 * 2 + 1], Complex::new(0., 0.));
     }
     #[test]
-    fn test_operator_Z() {
+    fn test_operator_z() {
         let z_gate = Operator::one_qubit(OneQubitOp::Z);
         assert_eq!(z_gate.data[0 * 2 + 0], Complex::new(1., 0.));
         assert_eq!(z_gate.data[0 * 2 + 1], Complex::new(0., 0.));
@@ -38,7 +38,7 @@ mod tests_operators {
         assert_eq!(z_gate.data[1 * 2 + 1], Complex::new(-1., 0.));
     }
     #[test]
-    fn test_operator_CX() {
+    fn test_operator_cx() {
         let cx_gate = Operator::two_qubits(TwoQubitsOp::CX);
         assert_eq!(cx_gate.data[0 * 4 + 0], Complex::new(1., 0.));
         assert_eq!(cx_gate.data[1 * 4 + 1], Complex::new(1., 0.));
@@ -46,7 +46,7 @@ mod tests_operators {
         assert_eq!(cx_gate.data[3 * 4 + 2], Complex::new(1., 0.));
     }
     #[test]
-    fn test_operator_CZ() {
+    fn test_operator_cz() {
         let cz_gate = Operator::two_qubits(TwoQubitsOp::CZ);
         assert_eq!(cz_gate.data[0 * 4 + 0], Complex::new(1., 0.));
         assert_eq!(cz_gate.data[1 * 4 + 1], Complex::new(1., 0.));
@@ -54,7 +54,7 @@ mod tests_operators {
         assert_eq!(cz_gate.data[3 * 4 + 3], Complex::new(-1., 0.));
     }
     #[test]
-    fn test_operator_SWAP() {
+    fn test_operator_swap() {
         let swap_gate = Operator::two_qubits(TwoQubitsOp::SWAP);
         assert_eq!(swap_gate.data[0 * 4 + 0], Complex::new(1., 0.));
         assert_eq!(swap_gate.data[1 * 4 + 2], Complex::new(1., 0.));
